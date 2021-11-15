f41f106f47e28bd3d387178c0c031605796c0338
pvburleigh <phba19ad@student.cbs.dk>
2021-11-15 10:03:38 +0100

    scroll wheels

---
+++ components/EducationSearch.js
@@ -0,0 +1,115 @@
+import React, {useEffect, useState} from "react";
+import firebase from "firebase";
+import {StyleSheet, Text, View, Button} from "react-native";
+import {Picker} from "@react-native-picker/picker";
+
+const EducationSearch = ({navigation}) => {
+    const [universities, setUniversities] = useState([]);
+    const [universityIndex, setUniversityIndex] = useState("initial");
+
+    const [educations, setEducations] = useState([]);
+    const [educationOptions, setEducationOptions] = useState([])
+    const [educationIndex, setEducationIndex] = useState("initial");
+
+    useEffect(()=>{
+        try {
+            firebase
+                .database()
+                .ref(`/universiteter`)
+                .on("value", (snapshot) => {
+                    setUniversities(snapshot.val());
+                });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, [educationOptions, educationIndex])
+
+    if(!universities) {
+        return (
+            <Text>Loading...</Text>
+        )
+    }
+
+    const uniOptions = universities.map((uni, index)=>{
+        return <Picker.Item label={uni.navn} value={index} key={index} />
+    })
+
+    const universitySelected = (value) => {
+        if(value !== 'initial'){
+            setUniversityIndex(value)
+            setEducationIndex('initial')
+            setEducations(universities[value].uddannelser)
+            setEducationOptions(universities[value].uddannelser.map((edu, index) => {
+                return <Picker.Item label={edu.navn} value={index} key={index} />
+            }))
+        } else {
+            setEducationOptions([])
+            setUniversityIndex('initial')
+            setEducationIndex('initial')
+        }
+
+    }
+
+    const educationSelected = (value) => {
+        setEducationIndex(value)
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <View style={{flex: 1, alignItems: 'center',
+                justifyContent: 'center'}}>
+                <View style={{flex: 1, alignItems: 'center',
+                    justifyContent: 'center'}}>
+                    <Text style={{fontSize: 18}}>Universitet</Text>
+                    <Picker
+                        selectedValue={universityIndex}
+                        style={{height: 10, width: 350 }}
+                        onValueChange={universitySelected}>
+                        <Picker.Item label="Vælg universitet" value="initial" key="initial" />
+                        {uniOptions}
+                    </Picker>
+                </View>
+
+                <View style={{flex: 1, alignItems: 'center',
+                    justifyContent: 'center'}}>
+                    { educationOptions.length ?
+                        <>
+                            <Text style={{fontSize: 18}}>Vælg uddannelse</Text>
+                            <Picker
+                                selectedValue={educationIndex}
+                                style={{flex: 1, width: 250 }}
+                                onValueChange={educationSelected}>
+                                <Picker.Item label="Vælg uddannelse" value="initial" key="initial" />
+                                {educationOptions}
+                            </Picker>
+                            <View style={{flex: 1}}>
+                                {educationIndex !== "initial" ?
+                                    <Button
+                                        title="Vis beretninger"
+                                    />
+                                    : null
+                                }
+                            </View>
+                        </>
+                        : null}
+                </View>
+            </View>
+            <View style={{alignItems: 'center',
+                justifyContent: 'center'}}>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default EducationSearch
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,64 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, StyleSheet, SafeAreaView } from "react-native";
-import { Picker } from '@react-native-picker/picker'
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+import EducationSearch from './EducationSearch.js'
 
 const WelcomeScreen = (props) => {
-
   const Stack = createStackNavigator();
-
-  const UniversitiesScrollWheel = ({navigation}) => {
-      const [university, setUniversity] = useState("initial");
-      const [education, setEducation] = useState("initial");
-      return (
-          <View style={styles.container}>
-              <View style={{flex:1}}>
-                  <Text style={{fontSize: 18,}}>Vælg universitet</Text>
-                  <Picker
-                      selectedValue={university}
-                      style={{height: 10, width: 250 }}
-                      onValueChange={(itemValue, itemIndex) => setUniversity(itemValue)}
-                  >
-                      <Picker.Item label="-" value="initial" />
-                      <Picker.Item label="JavaScript" value="js" />
-                  </Picker>
-              </View>
-
-              <View style={{flex:1}}>
-                  <Text style={{fontSize: 18,}}>Vælg uddannelse</Text>
-                  <Picker
-                      selectedValue={education}
-                      style={{height: 10, width: 250 }}
-                      onValueChange={(itemValue, itemIndex) => setEducation(itemValue)}
-                  >
-                      <Picker.Item label="-" value="initial" />
-                      <Picker.Item label="JavaScript" value="js" />
-                  </Picker>
-              </View>
-          </View>
-      );
-  }
     return(
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name={'Universities'} component={UniversitiesScrollWheel}/>
+                <Stack.Screen name={'Universities'} component={EducationSearch}/>
 
             </Stack.Navigator>
         </NavigationContainer>
     )
 
-
-
   // Når uddannelse er valgt, mount Education komponent, med valgte uddannelse som prop
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 40,
-        alignItems: "center"
-    }
-});
+
 export default WelcomeScreen;
